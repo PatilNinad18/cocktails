cc11393cf98860ca0069b0a3daf4067652172b00
PatilNinad18 <iampatil1804@gmail.com>
2026-01-04 15:52:25 +0530

    Created hero and cocktails section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
 
 gsap
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className='flex-center h-[100vh]'>
-        <h1 className='text-3xl text-indigo-300'>Hello GSAP</h1>
-    </div>
+    <main>
+        <Navbar/>
+        <Hero/>
+        <Cocktails/>
+    </main>
   )
 }
 
